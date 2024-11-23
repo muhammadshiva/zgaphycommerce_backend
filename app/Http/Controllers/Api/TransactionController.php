e666f66fd543d45b9260da8e5a77d6b06d3f0797
shivamuhammad <shiva@venturology.id>
2024-11-23 19:25:31 +0700

    init transaction controller api

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Transaction\Store;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    //
+    private function _fullyBookedChecker(Store $request) {}
 }
